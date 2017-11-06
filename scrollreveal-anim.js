a9214b94abbfdea6bb2fcfa593050a5cd1c19c35
miradontsoa <miradontsoa@gmail.com>
2017-11-06 15:37:15 +0300

    Create scrollreveal-anim.js
    First init : up, down, zoomin

---
+++ scrollreveal-anim.js
@@ -0,0 +1,67 @@
+// Scroll reveal
+window.sr = ScrollReveal();
+// up
+var animDistUp = '64px';
+sr.reveal('.sr-up-td1', {
+    delay: 100,
+    scale: 1,
+    distance: animDistUp,
+});
+sr.reveal('.sr-up-td2', {
+    delay: 200,
+    scale: 1,
+    distance: animDistUp,
+});
+sr.reveal('.sr-up-td3',{
+    delay: 300,
+    scale: 1,
+    distance: animDistUp,
+});
+sr.reveal('.sr-up-td4',{
+    delay: 400,
+    scale: 1,
+    distance: animDistUp,
+});
+// down
+var animDistDown = '-64px';
+sr.reveal('.sr-down-td1', {
+    delay: 100,
+    scale: 1,
+    distance: animDistDown,
+});
+sr.reveal('.sr-down-td2', {
+    delay: 200,
+    scale: 1,
+    distance: animDistDown,
+});
+sr.reveal('.sr-down-td3',{
+    delay: 300,
+    scale: 1,
+    distance: animDistDown,
+});
+sr.reveal('.sr-down-td4',{
+    delay: 400,
+    scale: 1,
+    distance: animDistDown,
+});
+// zoomin
+sr.reveal('.sr-zoomin-td1',{
+    delay: 100,
+    scale: 0.9,
+    distance: '0',
+});
+sr.reveal('.sr-zoomin-td2',{
+    delay: 200,
+    scale: 0.9,
+    distance: '0',
+});
+sr.reveal('.sr-zoomin-td3',{
+    delay: 300,
+    scale: 0.9,
+    distance: '0',
+});
+sr.reveal('.sr-zoomin-td4',{
+    delay: 400,
+    scale: 0.9,
+    distance: '0',
+});
